f0cb12a7521a205ea93ac6e25b8ae577610022ea
Chase-Lane <32445614+Chase-Lane@users.noreply.github.com>
2023-02-21 15:29:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,47 +30,28 @@ $(document).ready(function () {
      {
         $('.saveBtn').click(function () {
             let hour8 = document.getElementById('8').value;
-			localStorage.setItem('text8', hour8);
+			localStorage.setItem('8', hour8);
 			let hour9 = document.getElementById('9').value;
-			localStorage.setItem('text9', hour9);
+			localStorage.setItem('9', hour9);
 			let hour10 = document.getElementById('10').value;
-			localStorage.setItem('text10', hour10);
+			localStorage.setItem('t10', hour10);
 			let hour11 = document.getElementById('11').value;
-			localStorage.setItem('text11', hour11);
+			localStorage.setItem('t11', hour11);
 			let hour12 = document.getElementById('12').value;
-			localStorage.setItem('text12', hour12);
+			localStorage.setItem('t12', hour12);
 			let hour13 = document.getElementById('13').value;
-			localStorage.setItem('text13', hour13);
+			localStorage.setItem('t13', hour13);
 			let hour14 = document.getElementById('14').value;
-			localStorage.setItem('text14', hour14);
+			localStorage.setItem('t14', hour14);
 			let hour15 = document.getElementById('15').value;
-			localStorage.setItem('text15', hour15);
+			localStorage.setItem('t15', hour15);
 			let hour16 = document.getElementById('16').value;
-			localStorage.setItem('text16', hour16);
+			localStorage.setItem('t16', hour16);
 			let hour17 = document.getElementById('17').value;
-			localStorage.setItem('text17', hour17);
+			localStorage.setItem('t17', hour17);
         });
      }
 
   
-     let saved8 = localStorage.getItem('text8');
-     document.getElementById('8').value = saved8;
-     let saved9 = localStorage.getItem('text9');
-     document.getElementById('9').value = saved9;
-     let saved10 = localStorage.getItem('text10');
-     document.getElementById('10').value = saved10;
-     let saved11 = localStorage.getItem('text11');
-     document.getElementById('11').value = saved11;
-     let saved12 = localStorage.getItem('text12');
-     document.getElementById('12').value = saved12;
-     let saved13 = localStorage.getItem('text13');
-     document.getElementById('13').value = saved13;
-     let saved14 = localStorage.getItem('text14');
-     document.getElementById('14').value = saved14;
-     let saved15 = localStorage.getItem('text15');
-     document.getElementById('15').value = saved15;
-     let saved16 = localStorage.getItem('text16');
-     document.getElementById('16').value = saved16;
-     var saved17 = localStorage.getItem('text17');
-     document.getElementById('17').value = saved17;
+
 });
